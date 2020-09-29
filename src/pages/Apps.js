823892ab64602f5d4a33ec6668aeff8f7629f099
Jon Silver <js@jfdi.info>
2020-09-29 14:48:00 +0100

    improved Apps comp; new People comp WIP

--- src/pages/Apps.js
+++ src/pages/Apps.js
@@ -3,24 +3,30 @@ import React, { useState } from "react";
 import People from "./People";
 import ToDos from "./ToDos";
 
-const Apps = () => {
-    const [currentApp, loadApp] = useState("People");
-    const apps = { People: <People />, "To Do": <ToDos /> };
+const firstKey = (o = {}) => Object.keys(o)[0];
+const dictionaryToObjectArray = (o = {}, fn) => Object.entries(o).map(fn);
 
+const Apps = () => {
+    // A dictionary of apps is transformed into an array of Nav links.
+    const apps = { People: <People />, "To Do": <ToDos /> };
+    const links = dictionaryToObjectArray(apps, ([name]) => ({ name }));
     const navLinks = [
         {
-            links: Object.keys(apps).map((name) => ({ name }))
+            links
         }
     ];
-    const linkClicked = (e, link) => loadApp(link.name);
+    console.log(links);
+    const [currentApp, loadApp] = useState(firstKey(apps));
+
+    const linkClicked = (_, link) => loadApp(link.name);
 
     return (
-        <>
+        <div style={{ marginTop: "2rem", display: "flex" }}>
             <aside>
                 <Nav groups={navLinks} onLinkClick={linkClicked}></Nav>
             </aside>
             <article>{apps[currentApp]}</article>
-        </>
+        </div>
     );
 };
 
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,5 +1,7 @@
+import { Label, Persona, Shimmer, Stack } from "@fluentui/react";
 import React, { useEffect, useState } from "react";
 import { useTheme } from "../theming/ThemeContext";
+import { Card, CardItem, CardSection } from "@uifabric/react-cards";
 
 const People = () => {
     const [data, setData] = useState([]);
@@ -11,12 +13,40 @@ const People = () => {
     ] = useTheme();
 
     useEffect(() => {
-        fetch("https://fakerapi.it/api/v1/persons?_quantity=10")
-            .then((response) => response.json())
-            .then((obj) => setData(obj));
+        fetch("https://randomuser.me/api/?results=10")
+            .then(response => response.json())
+            .then(obj => setData(obj.results));
     }, []);
 
-    return <div>PEOPLE APP HERE</div>;
+    return (
+        <div>
+            <Stack horizontal wrap tokens={{ childrenGap: 10 }}>
+                {data.length ? (
+                    data.map(
+                        ({
+                            name: { first, last },
+                            email,
+                            picture: { thumbnail }
+                        }) => (
+                            <Card key={email}>
+                                <CardSection>
+                                    <CardItem>
+                                        <Persona
+                                            text={`${first} ${last}`}
+                                            secondaryText={email}
+                                            imageUrl={thumbnail}
+                                        />
+                                    </CardItem>
+                                </CardSection>
+                            </Card>
+                        )
+                    )
+                ) : (
+                    <Label text="No data to display" />
+                )}
+            </Stack>
+        </div>
+    );
 };
 
 export default People;
